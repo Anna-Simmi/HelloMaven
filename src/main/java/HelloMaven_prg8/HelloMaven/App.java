abdd9057c85c8aeb3f63136c07007e8fe154220c
DevOps <poojamahale726@gmail.com>
2025-05-29 10:49:07 +0530

    first commit

---
+++ src/main/java/HelloMaven_prg8/HelloMaven/App.java
@@ -0,0 +1,13 @@
+package HelloMaven_prg8.HelloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
